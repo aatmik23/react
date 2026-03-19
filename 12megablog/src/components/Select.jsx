3b7928374416dd3adb3aeeffb2891337bbfec642
aatmik23 <aleex2780@gmail.com>
2026-03-19 09:30:52 +0530

    added components

---
+++ 12megablog/src/components/Select.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Select({
+    lable,
+    options,
+    className,
+    ...props
+},ref) {
+    const id = useId()
+  return (
+    <div>
+        {label && <lable 
+        htmlFor={id}
+        className=''>
+            <Select
+            {...props}
+            id={id}
+            ref={ref}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+
+            > {
+                options.map((option)=>(
+                    <option key={option} value={option} >
+                    {option}
+                    </option>
+                ))
+            }
+            </Select>
+            </lable>}
+    </div>
+  )
+}
+
+export default React.forwardRef(Select)
--- 12megablog/src/components/index.js
+++ 12megablog/src/components/index.js
@@ -1,4 +1,11 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import Logoutbtn from "./header/Logoutbtn";
+import Button from "./Button";
+import Input from "./Input"
+export {
+    Header,Footer,Container,Logo,Logoutbtn,Button,Input
 
-export {Header,Footer}
+}
